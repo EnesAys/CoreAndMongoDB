950a46dc9d5f8e0d785c1934fea5e8f489b54328
Enes Aysan <enesaysan8@gmail.com>
2018-03-05 05:58:02 +0300

    Wep Api Security with JWT Token

---
+++ CoreAndMongoDB.UI/GlobalVariables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+
+namespace CoreAndMongoDB.UI
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("http://localhost:54206/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            /* Api Client içerisinde Default header bilgisi göndererek Authorizationu sağlamak.
+            //WebApiClient.DefaultRequestHeaders.Add("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiRW5lcyIsImV4cCI6MTUyMDIxOTg4OCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1NDIwNi8iLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjU0MjA2LyJ9.qKVNmnEePG9e0vliJcpVbLqjZ47mxysC9e3uA_iKiVc");
+            */
+        }
+      
+
+    }
+}
---
+++ CoreAndMongoDB/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using CoreAndMongoDB.DbModels;
+using CoreAndMongoDB.IRepository;
+using CoreAndMongoDB.Repository;
+
+
+namespace CoreAndMongoDB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           /* Api Security with JWT Token
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidateIssuerSigningKey = true,
+                       ValidIssuer = "http://localhost:54206/",
+                       ValidAudience = "http://localhost:54206/",
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]))
+                   };
+               });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("TrainedStaffOnly",
+                    policy => policy.RequireClaim("CompletedBasicTraining"));
+            });
+            */
+            services.Configure<Settings>(Options =>
+            {
+                Options.ConnectionString = Configuration.GetSection("MongoConnection:ConnectionString").Value;
+                Options.Database = Configuration.GetSection("MongoConnection:Database").Value;
+            });
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            /* For JWT Token Authorization
+            app.UseAuthentication();
+            */
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
